ab58c865d3545df64db0777c52170add5ac11686
IsThisALis <gamemodebronze2@gmail.com>
2026-03-03 19:04:19 +0500

    feat(Renderable) - interface now uses textures No-texture interface will be added later

--- src/main/java/org/gfs/recore/core/Renderable.java
+++ src/main/java/org/gfs/recore/core/Renderable.java
@@ -1,12 +1,13 @@
 package org.gfs.recore.core;
 
 import org.gfs.recore.graphics.shaders.ShaderProgram;
+import org.gfs.recore.graphics.textures.Texture;
 
 public interface Renderable {
 
-  void draw(ShaderProgram shaderProgram);
+  void draw(ShaderProgram shaderProgram, Texture texture);
   
-  void init(float[] vertices, int[] indices);
+  void init(float[] vertices, int[] indices, boolean useTexture);
 
   void update(float[] vertices, int[] indices);
 
