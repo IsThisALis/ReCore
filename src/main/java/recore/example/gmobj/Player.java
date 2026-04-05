b130495397860e08e079534b2be8743299523b17
IsThisALis <gamemodebronze2@gmail.com>
2026-04-05 18:36:01 +0500

    feat(Player) - add jump mechanic to player

--- src/main/java/recore/example/gmobj/Player.java
+++ src/main/java/recore/example/gmobj/Player.java
@@ -15,15 +15,16 @@ public class Player {
   Time time = new Time();
   Input input = new Input();
 
-  float speed = 2.5f;
+  float speed = 5f;
   boolean jumping;
+  float velocity = 5f;
 
     float[] vertices = {
-    // x     y     z    u     v
--0.25f, 0.25f, 0.0f, 0.0f, 1.0f,
-0.25f, 0.25f, 0.0f, 1.0f, 1.0f,
-0.25f,-0.25f, 0.0f, 1.0f, 0.0f,
--0.25f,-0.25f, 0.0f, 0.0f, 0.0f
+    // x     y     z    u   v
+    -0.25f, 0.25f, 0f, 0f, 1f,
+    0.25f, 0.25f, 0f, 1f, 1f,
+    0.25f,-0.25f, 0f, 1f, 0f,
+    -0.25f,-0.25f, 0f, 0f, 0f
   };
 
   int[] indices = {
@@ -53,16 +54,7 @@ public class Player {
   }
 
   public void input() {
-    float x = 0.0f;
-    float y = 0.0f;
-
-    if(input.keyPressed(input.getKey("W"))) {
-      y=0.25f;
-    }
-
-    if(input.keyPressed(input.getKey("S"))) {
-      y=-0.25f;
-    }
+    float x = 0f;
 
     if(input.keyPressed(input.getKey("A"))) {
       x=-0.25f;
@@ -74,18 +66,27 @@ public class Player {
 
     if(input.keyPressed(input.getKey("E"))) {
       mesh.setScale(1.25f, 1.25f, 1.25f);
+      speed = 2.5f;
+      //velocity = 2.5f;
     }
-
-    /*if(input.keyReleased(input.getKey("E"))) {
-      mesh.setScale(0.75f, 0.75f, 0.75f);
-    }*/
 
     if(input.keyPressed(input.getKey("C"))) {
       mesh.setScale(0.25f, 0.25f, 0.25f);
+      speed = 7.25f;
+      //velocity = 7.5f;
     } 
 
     if(input.keyReleased(input.getKey("C")) && input.keyReleased(input.getKey("E"))) {
       mesh.setScale(0.75f, 0.75f, 0.75f);
+      speed = 5f;
+    }
+
+    if(input.keyPressed(input.getKey("SPACE"))) {
+      jumping = true;
+    }
+
+    if(input.keyReleased(input.getKey("SPACE")) && jumping == false) {
+      velocity = 5f;
     }
 
     if(input.getScrollActionY() == 1) {
@@ -98,8 +99,20 @@ public class Player {
       }
     }
 
-    //mesh.setPosition(camera.getPosition().x, camera.getPosition().y);
-    camera.move(x, y, speed, time.getDelta()); 
+    if(jumping==true) {  
+
+      velocity -= 9.8f * time.getDelta();
+      float newY = mesh.getPosition().y + velocity * time.getDelta();
+
+      if(newY <= 0f) {
+        newY = 0f;
+        jumping = false;
+      }
+
+      camera.setPosition(camera.getPosition().x, newY);
+    }
+
+    camera.move(x, 0f, speed, time.getDelta()); 
     mesh.setPosition(camera.getPosition().x, camera.getPosition().y);
 
     input.resetScrollActionY();
