c3ca27403476d8e7c08e9e006bfbe25dbef37d22
IsThisALis <gamemodebronze2@gmail.com>
2026-04-06 19:40:54 +0500

    chore(ObjectManager) - add multiple game objects

--- src/main/java/recore/example/gmobj/ObjectManager.java
+++ src/main/java/recore/example/gmobj/ObjectManager.java
@@ -10,6 +10,9 @@ public class ObjectManager {
   Floor floor = new Floor();
   Banana banana = new Banana();
   GrassBlock grassBlock = new GrassBlock();
+  GrassBlock grassBlock1 = new GrassBlock();
+  GrassBlock grassBlock2 = new GrassBlock();
+  GrassBlock grassBlock3 = new GrassBlock();
   Player player = new Player();
 
   Window window = Params.getWindowInst();
@@ -18,7 +21,11 @@ public class ObjectManager {
   public void init() {
     floor.create();
     banana.create();
+    banana.setPosition(0.5f, 0.5f);
     grassBlock.create();
+    grassBlock1.create();
+    grassBlock2.create();
+    grassBlock3.create();
     player.init();
 
     program = Resources.getShaderProgramMap().getObj(0);
@@ -30,7 +37,15 @@ public class ObjectManager {
  
     floor.draw();
     banana.draw();
+
+    grassBlock.setPosition(0f, -1.15f);
     grassBlock.draw();
+    grassBlock1.setPosition(0.5f, -1.15f);
+    grassBlock1.draw();
+    grassBlock2.setPosition(1f, -1.15f);
+    grassBlock2.draw();
+    grassBlock3.setPosition(1.5f, -1.15f);
+    grassBlock3.draw(); 
     player.draw();
   }
 }
