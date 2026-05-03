fdc1becfb480e9099652bcc7f15d67fb8041364e
IsThisALis <gamemodebronze2@gmail.com>
2026-05-03 19:16:29 +0500

    docs(Scene) - added javadoc & improved codestyle

--- recore-modules/recore/src/main/java/recore/graphics/render/scene/Scene.java
+++ recore-modules/recore/src/main/java/recore/graphics/render/scene/Scene.java
@@ -1,32 +1,68 @@
 package recore.graphics.render.scene;
 
+  // ReCore imports
 import recore.graphics.render.Mesh;
+
+  // Java imports 
 import java.util.ArrayList;
 
 public class Scene {
 
+    // Scene object 
   private ArrayList<Mesh> scene;
+    // Objects in scene
   private int objCount;
 
+
+      /**
+       * Setter for objects count 
+       * @param value Objects number
+       */
     public void setObjCount(int value) {
         objCount = value;
     }
+
+
+      /**
+       * Creates scene 
+       */ 
     public void create() {
         scene = new ArrayList<Mesh>(objCount);
     }
 
+
+      /**
+       * Adds Mesh to scene
+       * @param index Object index in scene 
+       * @param object Object to add in scene 
+       */
     public void addObj(int index, Mesh object) {
         scene.add(index, object);
     }
 
+
+      /**
+       * Removes object from scene
+       * @param index Object index 
+       */
     public void rmObj(int index) {
         scene.remove(index);
     }
 
+
+      /**
+       * Getter for object 
+       * @param index Object index
+       * @return Mesh object from scene 
+       */
     public Mesh getObj(int index) {
         return scene.get(index);
     }
 
+
+      /**
+       * Draws all objects from scene 
+       */
     public void draw() {
       for(int x=0; x!=scene.size(); x++) {
           getObj(x).draw();
