f92202ebdeed921b8c7cf681b2e449890445a0ca
IsThisALis <gamemodebronze2@gmail.com>
2026-05-03 17:43:57 +0500

    docs(Window.Params) - added javadoc & improved codestyle

--- recore-modules/recore/src/main/java/recore/graphics/window/Params.java
+++ recore-modules/recore/src/main/java/recore/graphics/window/Params.java
@@ -26,86 +26,145 @@ public class Params {
   private static Window window = new Window();
 
 
-      //Instance
-      //Params 
-
+      /**
+       * Getter for window parameters class 
+       * @return This class instance
+       */
     public static Params getParams() {
-        return params;
+      return params;
     }
 
+
+      /**
+       * Getter for Window class 
+       * @return Window instance
+       */ 
     public static Window getWindowInst() {
       return window;
     }
+    
 
-      //Long 
-      //window
-
+      /**
+       * Getter for GLFW window 
+       * @return Window to use in GLFW (long)
+       */ 
     public long getWindow() {
         return windowL;
     }
 
+
+       /**
+        * Setter for GLFW window 
+        * @param value GLFW window 
+        */ 
     public void setWindow(long value) {
         windowL = value;
     }
-    
-      //int 
-      //height 
 
+    
+      /**
+       * Setter for window height 
+       * @param value Window height
+       */ 
     public void setHeight(int value) {
         height = value;
     }
     
+    
+      /**
+       * Getter for window height 
+       * @return Window height 
+       */ 
     public int getHeight() {
         return height;
     }
     
-      //width
-
+      
+      /**
+       * Setter for window width
+       * @param value Window width
+       */ 
     public void setWidth(int value) {
         width = value;
     }
 
+
+      /**
+       * Getter for window width
+       * @return Window width
+       */
     public int getWidth() {
         return width;
     }
     
-      //frameRateLimit
-
+      
+      /**
+       * Getter for FPS limit (unused)
+       * @return FPS limit 
+       */ 
     public int getFrameRateLimit() {
       return frameRateLimit;
     }
 
+
+      /**
+       * Setter for FPS limit (unused)
+       * @param value FPS limit
+       */
     public void setFrameRateLimit(int value) {
       frameRateLimit = value;
     }
     
-      //String
-      //title 
-
+      
+      /**
+       * Setter for window title
+       * @param value String to set as window title 
+       */
     public void setTitle(String value) {
         title = value;
     }
 
+
+      /**
+       * Getter for window title 
+       * @return Window title 
+       */
     public String getTitle() {
         return title;
     }
-    
-      //boolean
-      //vsync
 
+    
+      /**
+       * Getter for vsync state (unused)
+       * @return Vsync state
+       */ 
     public boolean getVsyncStatus() {
         return vsync;
     }
 
+
+      /**
+       * Setter for vsync state
+       * @param value Vsync state 
+       */
     public void setVsyncStatus(boolean value) {
       vsync = value;
     }
 
-      //fullscreen
+
+      /**
+       * Getter for fullscreen state (unused)
+       * @return Fullscreen state 
+       */
     public boolean isFullscreen() {
       return fullscreen;
     }
 
+
+      /**
+       * Setter for fullscreen state (unused)
+       * @param value Fullscreen state 
+       */
     public void setFullscreen(boolean value) {
       fullscreen = value;
     }
