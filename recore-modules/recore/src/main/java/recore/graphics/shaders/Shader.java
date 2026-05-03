564cf4104422f57b1cfdb305ed2fa5c358eced4c
IsThisALis <gamemodebronze2@gmail.com>
2026-05-03 18:35:32 +0500

    docs(Shader) - added javadoc & improved codestyle

--- recore-modules/recore/src/main/java/recore/graphics/shaders/Shader.java
+++ recore-modules/recore/src/main/java/recore/graphics/shaders/Shader.java
@@ -9,20 +9,33 @@ public class Shader {
       // Shader identifier
     private int id;
       // String representing shader type 
-    private int shType;
+    private String shaderType;
     
     
-      // Custom. Uploads shader code to OpenGL
+      /**
+       * Custom. Uploads shader code to OpenGL
+       * @param source Shader code
+       */
     public void uploadSource(CharSequence source) {
         glShaderSource(id, source);
     }
-      // Custom. Deprecated! Use only if you made custom createShader() 
+
+
+      /**
+       * Custom. Use only if you made custom method to create shader 
+       * @param id Shader to compile 
+       */
     public void compile(int id) {
         glCompileShader(id);
         checkStatus(id);
     }
+
     
-      //Checks shader compile status 
+      /**
+       * Checks shader compile status 
+       * @param id Shader to check 
+       * @return Shader compile state 
+       */
     public boolean checkStatus(int id) {
     int status = glGetShaderi(id, GL_COMPILE_STATUS);
     if (status != GL_TRUE && id == 0) {
@@ -31,43 +44,62 @@ public class Shader {
       return true;
     }
 
-      //Deletes shader 
+
+      /**
+       * Transforms string to int shader type 
+       * @param type Text shader type (vertex/fragment)
+       * @return OpenGL shader type 
+       */
+    public int getShaderType(String type) {
+      shaderType = type;
+        // Checks shader type
+      if (type.toLowerCase().contains("vertex")) {
+          return GL_VERTEX_SHADER;
+      }
+      if (type.toLowerCase().contains("fragment")) {
+          return GL_FRAGMENT_SHADER;
+      } else {
+            // Throwss exception when type invalid
+          throw new IllegalArgumentException("ReCore: Unknown shader type: "+type);
+        }
+    }
+
+
+      /**
+       * Deletes shader 
+       * @param id shader to be deleted 
+       */
     public void deleteShader(int id) {
         glDeleteShader(this.id);
     }
-      //returns shader id 
-    public int getID() {
+
+
+      /**
+       * Getter for shader identifier 
+       * @return Shader identifier 
+       */
+    public int getId() {
         return id;
     }
+
+
       /**
       * Creates shader from source and type 
       * @param type shader type - vertex or fragment
       * @param source code of your shader
       */
-    public void createShader(String type, String source) {
-            // Creates shader with type
-        if (type.contains("vertex")) {
-            shType = GL_VERTEX_SHADER;
-            id = glCreateShader(shType);
-        } else if (type.contains("fragment")) {
-            shType = GL_FRAGMENT_SHADER;
-            id = glCreateShader(shType);
-        } else {
-                // Throwss exception when type invalid
-            throw new IllegalArgumentException("Unknown shader type: "+type);
-        }
-
+    public void createShader(String source) {
+        id = glCreateShader(getShaderType(shaderType));
             // Check to be sure shader created
         if(id==0) {
-          throw new RuntimeException("Unable to create shader with type: "+type); 
+          throw new RuntimeException("ReCore: Unable to create shader with type: "+shaderType); 
         }
-
             // Uploads shader code and compiles
         glShaderSource(id, source);
         glCompileShader(id);
             // Check to be sure shader compiled
         if(!checkStatus(id)) {
-          throw new RuntimeException("Unable to compile shader. Type: "+type+" Check your shader code! Source: "+source);
+          throw new RuntimeException("ReCore: Unable to compile shader. Type: "+shaderType+" Check your shader code! Source: "+source);
         }
     }
 }
