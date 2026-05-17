cab326e424a86583f301f7f0e8a02b0fd5407610
IsThisALis <gamemodebronze2@gmail.com>
2026-05-17 16:03:31 +0500

    feat(Time) - add frame delta time and optimize hot paths

--- recore-modules/recore/src/main/java/recore/util/Time.java
+++ recore-modules/recore/src/main/java/recore/util/Time.java
@@ -2,16 +2,33 @@ package recore.util;
 
 public class Time {
 
-  Long lastTime = 0L;
+  long now;
+  long lastTime = 0L;
   float delta;
+
+  long start;
+  long end;
+  long elapsed;
+
   
   public void tick() {
-    long now = System.nanoTime();
+    now = System.nanoTime();
     delta = (now - lastTime) / 1_000_000_000f;
     lastTime = now;
   }
 
   public float getDelta() {
     return delta;
-  } 
+  }
+
+
+    public void startFrame() {
+        start = System.nanoTime();
+    }
+
+    public long endFrame() {
+        end = System.nanoTime();
+        elapsed = end - start;
+        return elapsed;
+    }
 }
