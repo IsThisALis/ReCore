288d08c08d8210ea150230fbfb26f51925c434f4
IsThisALis <gamemodebronze2@gmail.com>
2026-05-24 21:34:14 +0500

    style(Collizion) - improve codestyle

--- recore-modules/rexample/src/main/java/rexample/interaction/Collizion.java
+++ recore-modules/rexample/src/main/java/rexample/interaction/Collizion.java
@@ -12,24 +12,24 @@ public class Collizion implements ComponentLogic {
 
     private static boolean playerState = false;
 
-    @Override // recore.core.ComponentLogic
+      @Override
     public void init() {
-        this.banana.setMin(0.5f, -1.0f);
-        this.banana.setMax(1.0f, -0.5f);
+        banana.setMin(0.5f, -1.0f);
+        banana.setMax(1.0f, -0.5f);
 
-        this.player.setMin(0.5f, -1f);
-        this.player.setMax(1f, -0.5f);
+        player.setMin(0.5f, -1f);
+        player.setMax(1f, -0.5f);
     }
 
-    @Override // recore.core.ComponentLogic
+      @Override
     public void update() {
-        playerState = AABB.AABBOverLap(this.banana, this.player, Resources.getScene().getObj(0), Resources.getScene().getObj(3));
+        playerState = AABB.AABBOverLap(banana, player, Resources.getScene().getObj(0), Resources.getScene().getObj(3));
     }
 
-    @Override // recore.core.ComponentLogic
+      @Override
     public void cleanup() {
-        this.banana = null;
-        this.player = null;
+        banana = null;
+        player = null;
     }
 
     public static boolean getPlayerState() {
