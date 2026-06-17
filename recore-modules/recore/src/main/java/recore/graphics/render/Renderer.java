fbea76e84f576f7341edffdc82be6af76d505ffc
IsThisALis <gamemodebronze2@gmail.com>
2026-06-17 09:46:08 +0500

    Reapply "feat(Render) - update optimized rendering algorithm"
    This reverts commit b67bf6e3741b7bd4bf274b2c4bf878a2949f44d1.

--- recore-modules/recore/src/main/java/recore/graphics/render/Renderer.java
+++ recore-modules/recore/src/main/java/recore/graphics/render/Renderer.java
@@ -2,6 +2,7 @@ package recore.graphics.render;
 
 import recore.graphics.camera.Camera;
 import recore.graphics.camera.FOV;
+import recore.graphics.window.Params;
 
 public class Renderer {
 
@@ -9,7 +10,9 @@ public class Renderer {
 
   public static boolean draw(Mesh mesh, Camera camera) {
     if(fov.inFov(mesh, camera)) {
+      Params.getWindowInst().blend(true);
       mesh.draw();
+      Params.getWindowInst().blend(false);
       return true;
     } else { return false; }
   }
