79b89b02dbf83185b08c243aa1b26e57941ad82a
IsThisALis <gamemodebronze2@gmail.com>
2026-08-23 19:03:05 +0500

    refactor(Texture): major changes in class structure, prepare for major update

--- recore-modules/recore/src/main/java/com/isthisalis/recore/graphics/textures/Texture.java
+++ recore-modules/recore/src/main/java/com/isthisalis/recore/graphics/textures/Texture.java
@@ -1,48 +1,39 @@
 package com.isthisalis.recore.graphics.textures;
 
-  // graphic imports
+  // ReCore imports
 import com.isthisalis.recore.graphics.shaders.ShaderProgram;
+import com.isthisalis.recore.graphics.shaders.uniforms.TextureUniform;
 
+  // Lombok imports
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-// OpenGL imports
+  // OpenGL imports.
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL13.GL_CLAMP_TO_BORDER;
+import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
 import static org.lwjgl.opengl.GL30.glGenerateMipmap;
 
 import java.nio.ByteBuffer;
 
-// STB imports
+  // STB imports.
 import org.lwjgl.stb.STBImage;
 
-  //@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class Texture {
 
     /**
      * Number storing textures number.
      */
-  private static int textures = 0;
+  private @Getter static int textures = 0;
       // Used to set uniform
-  private int location;
-      // Parameters instance
-  private Params params;
-
-  private int width;
-  private int height;
-  private ByteBuffer image; 
-
-
-  public Texture(int width, int height, ByteBuffer image) {
-    this.width = width;
-    this.height = height;
-    this.image = image;
-  }
-
-
-    @Deprecated
-  public Texture() {
-    params = new Params();
-  }
+  private @Getter int id;
+    
+  private final @Getter int width;
+  private final @Getter int height;
+  private final ByteBuffer image;
+  private @Getter TextureUniform uniform;
  
 
   /** 
@@ -50,35 +41,35 @@ public class Texture {
    */
   public void bind() {
     glActiveTexture(GL_TEXTURE0 + textures);
-    glBindTexture(GL_TEXTURE_2D, params.getId());
+    glBindTexture(GL_TEXTURE_2D, id);
     textures++;
   }
 
 
   /**
-   * Deletes texture, full cleanup
+   * Deletes texture.
    */
   public void delete() {
-    glDeleteTextures(params.getId());
-    params = null;
+    glDeleteTextures(id);
   }
 
 
   /**
-   * Custom. Uploads setted and loaded data into texture
+   * Uploads setted and loaded data into texture
    */
   public void uploadData() {
-     params.getImage().position(0);
-     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, params.getWidth(), params.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, params.getImage());
+     image.position(0);
+     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
   }
 
 
   /**
-   * Fully creates texture if image loaded
-   * @param program ShaderProgram with shaders to use with texture
+   * Fully creates texture if image loaded.
+   * @param program ShaderProgram with shaders to use with texture.
+   * @param uniformName Texture uniform name in shader.
    */
-  public void createTexture(ShaderProgram program) {
-    params.setId(glGenTextures());
+  public void createTexture(ShaderProgram program, String uniformName) {
+    id = glGenTextures();
 
     bind();
           // Texture parameters
@@ -94,27 +85,22 @@ public class Texture {
 
       // Setting uniform
     program.use(); 
-    location = glGetUniformLocation(program.getId(), params.getUniformName());
-    glUniform1i(location, 0);
+    uniform = new TextureUniform(
+      glGetUniformLocation(program.getId(), uniformName),
+      GL_TEXTURE0 + (textures - 1),
+      uniformName);
+
+    glUniform1i(uniform.location(), 0);
     
       // Cleans image buffer
-    STBImage.stbi_image_free(params.getImage());
+    STBImage.stbi_image_free(image);
   }
 
 
     /**
-     * Updates texture uniform
+     * Updates texture uniform.
      */
   public void update() {
-    glUniform1i(location, 0);
-  }
-
-
-    /**
-     * Getter for texture parameters 
-     * @return Parameters class instance 
-     */
-  public Params getParams() {
-    return params;
+    glUniform1i(uniform.location(), 0);
   }
 }
