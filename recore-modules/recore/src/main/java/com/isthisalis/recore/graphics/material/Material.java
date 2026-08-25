2870289a6ffd5fec09adab2d5a9dbafdca631885
IsThisALis <gamemodebronze2@gmail.com>
2026-08-25 09:48:02 +0500

    chore(Material): add Lombok setters & getters

--- recore-modules/recore/src/main/java/com/isthisalis/recore/graphics/material/Material.java
+++ recore-modules/recore/src/main/java/com/isthisalis/recore/graphics/material/Material.java
@@ -4,6 +4,7 @@ import com.isthisalis.recore.graphics.shaders.ShaderProgram;
 import com.isthisalis.recore.graphics.textures.Texture;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
@@ -14,10 +15,9 @@ import lombok.Setter;
 public class Material {
 
   private @Getter @Setter float scaleX, scaleY;
-  private @Getter @Setter boolean transparent = false;
 
-  private final Texture texture;
-  private final ShaderProgram program;
+  private @NonNull @Getter @Setter Texture texture;
+  private @NonNull @Getter @Setter ShaderProgram program;
 
 
   public void setScale(float x, float y) {
