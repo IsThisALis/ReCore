afffee3c38754cd62c3c0c58870e45b030eb1449
IsThisALis <gamemodebronze2@gmail.com>
2026-08-25 09:52:50 +0500

    WiP(Scene): start scene rework

--- recore-modules/recore/src/main/java/com/isthisalis/recore/graphics/render/scene/Scene.java
+++ recore-modules/recore/src/main/java/com/isthisalis/recore/graphics/render/scene/Scene.java
@@ -1,71 +1,28 @@
 package com.isthisalis.recore.graphics.render.scene;
 
-  // ReCore imports
-import com.isthisalis.recore.graphics.render.Mesh;
-
-  // Java imports 
 import java.util.ArrayList;
+import java.util.List;
 
+import com.isthisalis.recore.graphics.camera.Camera;
+import com.isthisalis.recore.graphics.render.Entity;
+import com.isthisalis.recore.graphics.render.Renderer;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Scene
+ */
+@RequiredArgsConstructor
 public class Scene {
 
-    // Scene object 
-  private ArrayList<Mesh> scene;
-    // Objects in scene
-  private int objCount;
+  private @Getter List<Entity> entities = new ArrayList<>();
+  private @NonNull Camera camera;
 
-
-      /**
-       * Setter for objects count 
-       * @param value Objects number
-       */
-    public void setObjCount(int value) {
-        objCount = value;
+  public void render() {
+    for (Entity entity : entities) {
+      Renderer.render(entity, camera);
     }
-
-
-      /**
-       * Creates scene 
-       */ 
-    public void create() {
-        scene = new ArrayList<Mesh>(objCount);
-    }
-
-
-      /**
-       * Adds Mesh to scene
-       * @param index Object index in scene 
-       * @param object Object to add in scene 
-       */
-    public void addObj(int index, Mesh object) {
-        scene.add(index, object);
-    }
-
-
-      /**
-       * Removes object from scene
-       * @param index Object index 
-       */
-    public void rmObj(int index) {
-        scene.remove(index);
-    }
-
-
-      /**
-       * Getter for object 
-       * @param index Object index
-       * @return Mesh object from scene 
-       */
-    public Mesh getObj(int index) {
-        return scene.get(index);
-    }
-
-
-      /**
-       * Draws all objects from scene 
-       */
-    public void draw() {
-      for(int x=0; x!=scene.size(); x++) {
-          getObj(x).draw();
-      }
-    }
+  }
 }
