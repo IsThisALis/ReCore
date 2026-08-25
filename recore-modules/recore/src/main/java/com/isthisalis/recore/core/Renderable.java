6a1208ec5b4506f5eafa02f278c91fb9928ae911
IsThisALis <gamemodebronze2@gmail.com>
2026-08-25 09:20:41 +0500

    chore: reworking Renderable (WiP)

--- recore-modules/recore/src/main/java/com/isthisalis/recore/core/Renderable.java
+++ recore-modules/recore/src/main/java/com/isthisalis/recore/core/Renderable.java
@@ -1,43 +1,10 @@
 package com.isthisalis.recore.core;
 
-import com.isthisalis.recore.graphics.camera.Camera;
-import com.isthisalis.recore.graphics.material.Material;
-import com.isthisalis.recore.graphics.render.Mesh;
-import com.isthisalis.recore.graphics.render.Renderer;
+import com.isthisalis.recore.graphics.render.Entity;
+
+import lombok.Getter;
 
 public abstract class Renderable {
 
-  private Mesh mesh;
-  private Material material;
-  private Camera camera;
-
-
-  public Renderable(Mesh mesh, Material material, Camera camera) {
-    this.mesh = mesh;
-    this.material = material;
-    this.camera = camera;
-  }
-
-
-  public void render() {
-    Renderer.draw(mesh, material, camera);
-  } 
-
-
-  public void setMaterial(Material material) {
-    this.material = material;
-  }
-
-
-  public void setMesh(Mesh mesh) {
-    this.mesh = mesh;
-  }
-
-
-  public void setCamera(Camera camera) {
-    this.camera = camera;
-  }
-
-
-  public abstract void cleanup();
+    private @Getter Entity entity;
 }
