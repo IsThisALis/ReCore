d15c5422aa9557047c799314f4de127004ddda4d
IsThisALis <gamemodebronze2@gmail.com>
2026-08-27 10:53:14 +0500

    style: change constant name

--- recore-modules/recore/src/main/java/com/isthisalis/recore/util/OS.java
+++ recore-modules/recore/src/main/java/com/isthisalis/recore/util/OS.java
@@ -2,17 +2,17 @@ package com.isthisalis.recore.util;
 
 public class OS {
 
-  private static final String os = System.getProperty("os.name").toLowerCase();
+  private static final String OS = System.getProperty("os.name").toLowerCase();
 
   public static boolean isWindows() {
-    return os.contains("win");
+    return OS.contains("win");
   }
 
   public static boolean isMac() {
-    return os.contains("mac");
+    return OS.contains("mac");
   }
 
   public static boolean isLinux() {
-    return os.contains("linux");
+    return OS.contains("linux");
   }
 }
