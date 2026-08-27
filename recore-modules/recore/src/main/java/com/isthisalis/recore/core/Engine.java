ca9d22c6d0f160f69178ce56bc08374d0c06e683
IsThisALis <gamemodebronze2@gmail.com>
2026-08-27 10:37:02 +0500

    feat(Engine): add window automatic control

--- recore-modules/recore/src/main/java/com/isthisalis/recore/core/Engine.java
+++ recore-modules/recore/src/main/java/com/isthisalis/recore/core/Engine.java
@@ -6,9 +6,12 @@ import com.isthisalis.recore.util.Time;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Engine work organizer class.
+ */
 public abstract class Engine {
 
-  private @Setter Window window;
+  private @Setter @Getter Window window;
   private final @Getter AssetManager assetManager = new AssetManager();
   private final @Getter Time time = new Time();
 
@@ -19,8 +22,12 @@ public abstract class Engine {
   public void loop() {
     while (!window.isWindowShouldClose()) {
       update();
+      window.clean();
+      window.update();
     }
+    
     assetManager.cleanup();
+    window.cleanup();
     cleanup();
   }
 }
